6f8c024b65d8a581e6e21fd7f5b5ebaf23b540e0
gorgonun <polariusvulcano@gmail.com>
2020-03-31 20:21:02 -0300

    simpsons

--- random_quote_machine/src/App.js
+++ random_quote_machine/src/App.js
@@ -15,22 +15,40 @@ class QuoteBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quote: "If you hear a voice within you say “you cannot paint,” then by all means paint and that voice will be silenced. ",
-            author: "Mrs. Catra Júnior"
+            quote: "",
+            author: "",
+            image: ""
         }
-        this.NewQuote = this.NewQuote.bind(this)
+        this.NewQuote = this.NewQuote.bind(this);
     }
     NewQuote() {
-
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        fetch(proxyurl + "https://thesimpsonsquoteapi.glitch.me/quotes")
+            .then(res => res.json())
+            .then(result => {
+                this.setState({
+                    quote: result[0].quote,
+                    author: result[0].character,
+                    image: result[0].image
+            })
+                document.body.style.backgroundImage = `url(${this.state.image})`;
+            });
     }
+    componentDidMount() {
+        this.NewQuote();
+    }
+
     render() {
         return (
             <div id="quote-box">
                 <p id="text">{this.state.quote}</p>
                 <p id="author">{this.state.author}</p>
                 <div id="bottom">
+                    <div id="social-networks">
+                        <a id="tweet-quote"></a>
+                    </div>
                     <div id="quote">
-                        <button id="new-quote" onClick={this.NewQuote}/>
+                        <button id="new-quote" onClick={this.NewQuote}>New quote</button>
                     </div>
                 </div>
                 <a id="tweet-quote"/>
